601c1ffadae5e24e13e0acce173d5259c54fc04c
NCA2212 <nichoalex1122@gmail.com>
2025-08-05 09:57:51 +0700

    Secure pw

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'wutdehelll'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/db_auth' #pakai password postgres sendiri
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/db_name' #pakai password postgres sendiri
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
